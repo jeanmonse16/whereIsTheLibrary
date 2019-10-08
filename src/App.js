53fcfe87f7670c4a42def62ae3b0ff1816b7a741
jeanpier <monserrateluisje@gmail.com>
2019-10-08 09:40:14 -0400

    xd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+const BACKGROUND_HEADER = 'https://steamuserimages-a.akamaihd.net/ugc/996892020308483531/67D7FE7EFB9B47F88F1BBDE47FBDF11489EA0E49/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true'
+
 export const App = () => (
   <div>
     <div>
-      
+      <img src={BACKGROUND_HEADER} />
     </div>
     <h1>Hola mundo</h1>
   </div>
